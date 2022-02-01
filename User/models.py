983cf4bcd523fdd3671b77c085de5cbe5c72ca9d
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-01 11:11:04 +0500

    updated models [added models, updated prev models]

--- User/models.py
+++ User/models.py
@@ -11,10 +11,22 @@ class Profile(models.Model):
     services = models.ManyToManyField('Services')
     enable_pricing = models.BooleanField(default=True)
     pricing = models.ManyToManyField('Pricing')
+    enable_testimonials = models.BooleanField(default=True)
+    testimonials = models.ManyToManyField("Testimonials")
+    enable_posts = models.BooleanField(default=True)
+    posts = models.ManyToManyField('Posts')
+
+
+class Testimonials(models.Model):
+    image = models.ImageField(upload_to='media/images/clients')
+    testimonial = models.TextField()
+    name = models.CharField(max_length=15)
 
 
 class Pricing(models.Model):
-    pass
+    plan_name = models.CharField(max_length=25, default="Basic")
+    description = models.CharField(max_length=255, default="Lorem ipsum")
+    price = models.FloatField(default=0.0)
 
 
 class Services(models.Model):
@@ -23,5 +35,9 @@ class Services(models.Model):
     image = models.ImageField(upload_to='media/images')
 
 
-class Blogs(models.Model):
-    pass
+class Posts(models.Model):
+    image = models.ImageField(upload_to='media/images/posts')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField(blank=True, null=True)
+
