8a10b2e0f0aa1524afe742acb5393d96413f84e6
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-01 11:01:43 +0500

    added models [Pricing, Services]

--- User/models.py
+++ User/models.py
@@ -7,6 +7,20 @@ class Profile(models.Model):
     main_designation = models.CharField(max_length=255, default="Web Developer")
     about = models.TextField()
     cv = models.FileField(upload_to='media/documents/')
+    enable_services = models.BooleanField(default=True)
+    services = models.ManyToManyField('Services')
+    enable_pricing = models.BooleanField(default=True)
+    pricing = models.ManyToManyField('Pricing')
+
+
+class Pricing(models.Model):
+    pass
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='media/images')
 
 
 class Blogs(models.Model):
