d6a00c136da03f5f7eba3c888142a23ef75c022b
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-09 08:09:26 +0500

    completed carousel

--- Main/templatetags/filters.py
+++ Main/templatetags/filters.py
@@ -4,5 +4,44 @@ register = template.Library()
 
 
 @register.filter(name='range_')
-def range_(number):
-    return range(number)
+def range_(number, diff=1):
+    return range(0, number, diff)
+
+
+@register.filter(name='querysetToList')
+def querysetToList(queryset, index):
+    if not index:
+        if queryset.count() > 0:
+            return [element for element in queryset]
+        return []
+    else:
+        list_ = [element for element in queryset]
+        return list_[index]
+
+
+@register.filter(name='quotient')
+def quotient(number, divideby):
+    return int(number / divideby)
+
+
+@register.filter(name='remainder')
+def remainder(number, divideby):
+    return number % divideby
+
+
+@register.filter(name='toGrid')
+def toGrid(querySet, column):
+    if querySet.count() > 0:
+        list_ = []
+        temp = []
+        i = 0
+        for element in querySet:
+            if not i == 0 and i % column == 0:
+                list_.append([temp, i])
+                temp = []
+            temp.append(element)
+            i += 1
+        if len(temp) > 0:
+            list_.append([temp, i])
+        return list_
+    return []
