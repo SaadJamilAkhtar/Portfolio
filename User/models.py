1064813035a3eeec6b1fdb925e311ec9f9981425
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-27 16:25:44 +0500

    Updated Posts model - removed url field

--- User/models.py
+++ User/models.py
@@ -77,7 +77,6 @@ class Posts(models.Model):
     image = models.ImageField(upload_to='images/posts')
     title = models.CharField(max_length=255)
     description = models.TextField()
-    url = models.URLField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         super(Posts, self).save(*args, **kwargs)
